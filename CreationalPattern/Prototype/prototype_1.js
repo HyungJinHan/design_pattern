1ed3a1cabd73038b6f862a60c6f22082d6cfa4ff
HyungJinHan <han1210_36@naver.com>
2024-04-25 17:15:05 +0900

    외부에서 prototype에 메소드 추가

--- CreationalPattern/Prototype/prototype_1.js
+++ CreationalPattern/Prototype/prototype_1.js
@@ -1,3 +1,4 @@
+// Prototype Pattern 기본
 let vehiclePrototype = {
   init: function (carModel) {
     this.model = carModel;
@@ -19,4 +20,31 @@ const vehicle = (model) => {
 
 let car = vehicle("Tesla Model X");
 
-car.getModel();
+car.getModel(); // Tesla Model X
+
+// 메소드를 외부에서 Prototype에 지정
+class Car {
+  constructor(make, model, level, color, warranty) {
+    this.make = make;
+    this.model = model;
+    this.level = level;
+    this.color = color;
+    this.color = color;
+    this.warranty = warranty;
+  }
+  getInfo() {
+    return console.log(
+      "----------------------------------------------------\n" +
+        `${this.make}, ${this.model}, ${this.level}, ${this.color}, ${this.warranty}`
+    );
+  }
+}
+
+let newCar = new Car("Tesla", "Model X", "Sedan", "White", "5 Year");
+
+Car.prototype.zeroback = function () {
+  return console.log("2.9 seconds");
+};
+
+newCar.getInfo(); // Tesla, Model X, Sedan, White, 5 Year
+newCar.zeroback(); // 2.9 seconds
