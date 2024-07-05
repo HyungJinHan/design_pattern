91df2d97877407c5549e07dc8e7761bfdbaa0b3c
HyungJinHan <han1210_36@naver.com>
2024-07-05 15:52:50 +0900

    홀짝 / 빗질 정렬 추가

---
+++ NonePattern/SortingAlgorithm/ExchangeSort/comb_sort.ts
@@ -0,0 +1,33 @@
+import { arr } from "../number_array";
+
+class combSort {
+  public static main(arr: number[]): void {
+    let sorted: boolean = true;
+    let i: number, gap: number, size: number;
+    let count: number = 0,
+      temp: number = 0;
+
+    gap = size = arr.length;
+
+    while (gap !== 1 || sorted) {
+      gap /= 1.3;
+
+      if (gap < 1) gap = 1;
+
+      sorted = false;
+
+      for (i = 0; i < size - gap; i++) {
+        if (arr[i] > arr[i + gap]) {
+          temp = arr[i];
+          arr[i] = arr[i + gap];
+          arr[i + gap] = temp;
+          sorted = true;
+        }
+      }
+      count++;
+      console.log(arr.toString(), `→ [${count}회]`);
+    }
+  }
+}
+
+combSort.main(arr);
---
+++ NonePattern/SortingAlgorithm/ExchangeSort/odd-even_sort.ts
@@ -0,0 +1,36 @@
+import { arr } from "../number_array";
+
+class oddEvenSort {
+  public static main(arr: number[]): void {
+    let sorted = false;
+    let i: number = 0;
+
+    const swap = (arr: number[], i: number, j: number): void => {
+      let temp: number = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    };
+
+    while (!sorted) {
+      sorted = true;
+      for (let j = 1; j < arr.length - 1; j += 2) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          sorted = false;
+        }
+      }
+
+      for (let j = 0; j < arr.length - 1; j += 2) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          sorted = false;
+        }
+      }
+      i++;
+      console.log(arr.toString(), `→ [${i}회]`);
+    }
+    // console.log(arr.toString());
+  }
+}
+
+oddEvenSort.main(arr);
