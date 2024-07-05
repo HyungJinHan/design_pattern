f6767ef089b34ddaa28e6b289aae6d4b6f9c08a3
HyungJinHan <han1210_36@naver.com>
2024-07-05 10:36:13 +0900

    버블 정렬 알고리즘 추가

---
+++ NonePattern/SortingAlgorithm/ExchangeSort/bubble_sort.ts
@@ -0,0 +1,20 @@
+import { arr } from "../number_array";
+
+class bubbleSort {
+  public static main(arr: number[]): void {
+    let temp = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < arr.length - i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          temp = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+      console.log(`${i}회`);
+      console.log(arr.toString());
+    }
+  }
+}
+
+bubbleSort.main(arr);
