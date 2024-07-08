62038f66950a1179684c1ea82b7a7e32b3650a3a
HyungJinHan <han1210_36@naver.com>
2024-07-08 10:05:55 +0900

    난쟁이 정렬 추가

---
+++ NonePattern/SortingAlgorithm/ExchangeSort/gnome_sort.ts
@@ -0,0 +1,30 @@
+import { arr } from "../number_array";
+
+class gnomeSort {
+  public static main(arr: number[]): void {
+    let pos: number = 0;
+    let i: number = 0;
+
+    const swap = (arr: number[], i: number, j: number): void => {
+      let temp: number = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    };
+
+    for (pos = 0; pos < arr.length - 1; ) {
+      if (pos < 0 || arr[pos] <= arr[pos + 1]) {
+        pos++;
+        continue;
+      }
+
+      swap(arr, pos, pos + 1);
+      pos--;
+
+      i++;
+      console.log(arr.toString(), `→ [${i}회]`);
+    }
+    // console.log(arr.toString());
+  }
+}
+
+gnomeSort.main(arr);
